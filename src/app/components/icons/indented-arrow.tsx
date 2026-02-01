acfc0ca186a8033a4be150d060976f0bc82ba15d
Nonso Anetoh <anet0001@algonquinlive.com>
2026-01-31 23:27:00 -0800

    add: indented arrow icon component

---
+++ src/app/components/icons/indented-arrow.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const IndentedArrowIcon = () => {
+  return (
+    <svg
+      viewBox="0 0 49 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      preserveAspectRatio="xMidYMid meet"
+    >
+      <path
+        d="M0.75 9.5H0V10.25H0.75V9.5ZM48.2803 10.0303C48.5732 9.73744 48.5732 9.26256 48.2803 8.96967L43.5074 4.1967C43.2145 3.90381 42.7396 3.90381 42.4467 4.1967C42.1538 4.48959 42.1538 4.96447 42.4467 5.25736L46.6893 9.5L42.4467 13.7426C42.1538 14.0355 42.1538 14.5104 42.4467 14.8033C42.7396 15.0962 43.2145 15.0962 43.5074 14.8033L48.2803 10.0303ZM0.75 0H0V9.5H0.75H1.5V0H0.75ZM0.75 9.5V10.25H47.75V9.5V8.75H0.75V9.5Z"
+        fill="#1FBC27"
+      />
+    </svg>
+  );
+};
+
+export default IndentedArrowIcon;
