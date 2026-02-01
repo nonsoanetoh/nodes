6cb372e7d09806598724ba66ae8c855516d92b64
Nonso Anetoh <anet0001@algonquinlive.com>
2026-01-31 23:26:55 -0800

    add: duplicate frame icon component

---
+++ src/app/components/icons/duplicate.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Duplicate = () => {
+  return (
+    <svg
+      viewBox="0 0 14 14"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      preserveAspectRatio="xMidYMid meet"
+    >
+      <rect
+        x="0.5"
+        y="13.5"
+        width="13"
+        height="13"
+        transform="rotate(-90 0.5 13.5)"
+        stroke="#0000FF"
+        strokeLinecap="square"
+        strokeDasharray="3 3"
+      />
+    </svg>
+  );
+};
+
+export default Duplicate;
