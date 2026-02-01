14f222008b899fdbd2e005a59223f2cbd053e28d
Nonso Anetoh <anet0001@algonquinlive.com>
2026-01-31 23:26:54 -0800

    add: remove frame icon component

---
+++ src/app/components/icons/remove.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Remove = () => {
+  return (
+    <svg
+      viewBox="0 0 18 4"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      preserveAspectRatio="xMidYMid meet"
+    >
+      <rect
+        y="4"
+        width="4"
+        height="17.8492"
+        transform="rotate(-90 0 4)"
+        fill="#FF0004"
+      />
+    </svg>
+  );
+};
+
+export default Remove;
