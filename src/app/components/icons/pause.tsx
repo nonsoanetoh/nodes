b31b13a196a3ff1de6c4a1197de9a5de6f5d91a5
Nonso Anetoh <anet0001@algonquinlive.com>
2026-01-31 23:26:50 -0800

    add: pause icon component

---
+++ src/app/components/icons/pause.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Pause = () => {
+  return (
+    <svg
+      viewBox="0 0 13 17"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      preserveAspectRatio="xMidYMid meet"
+    >
+      <path d="M0 0H4V17H0V0Z" fill="#595959" />
+      <path d="M9 0H13V17H9V0Z" fill="#595959" />
+    </svg>
+  );
+};
+
+export default Pause;
