25087c3283b2dc4f4ec4e1039dd0599e471965c5
Nonso Anetoh <anet0001@algonquinlive.com>
2026-01-31 23:26:52 -0800

    add: next frame icon component

---
+++ src/app/components/icons/next.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Next = () => {
+  return (
+    <svg
+      viewBox="0 0 18 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      preserveAspectRatio="xMidYMid meet"
+    >
+      <path d="M17.8492 8L5.67931 16V0L17.8492 8Z" fill="#595959" />
+      <path d="M4.05665 16H0V0H4.05665V16Z" fill="#595959" />
+    </svg>
+  );
+};
+
+export default Next;
