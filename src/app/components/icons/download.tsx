c0a0a2270972e7318122818b104b2eeb76bb5553
Nonso Anetoh <anet0001@algonquinlive.com>
2026-01-31 23:26:58 -0800

    add: download icon component

---
+++ src/app/components/icons/download.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Download = () => {
+  return (
+    <svg
+      viewBox="0 0 16 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      preserveAspectRatio="xMidYMid meet"
+    >
+      <path
+        d="M8 11L3 6H6V1H10V6H13L8 11Z"
+        fill="#595959"
+        stroke="#595959"
+        strokeWidth="1"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M2 14H14"
+        stroke="#595959"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+      />
+    </svg>
+  );
+};
+
+export default Download;
