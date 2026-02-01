3dc449d25edeaecd24dc3b2849587541564098e8
Nonso Anetoh <anet0001@algonquinlive.com>
2026-01-31 23:27:01 -0800

    add: add image icon component

---
+++ src/app/components/icons/add-image.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const AddImage = () => {
+  return (
+    <svg
+      viewBox="0 0 76 68"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      preserveAspectRatio="xMidYMid meet"
+    >
+      <path d="M37.9296 0V68" stroke="#786F6F" strokeWidth="1.5" />
+      <path d="M0 34L75.8593 34" stroke="#786F6F" strokeWidth="1.5" />
+    </svg>
+  );
+};
+
+export default AddImage;
