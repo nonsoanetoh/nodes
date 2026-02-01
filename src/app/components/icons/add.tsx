d76b8a5c9918df447fc9b691c204674e6e26e013
Nonso Anetoh <anet0001@algonquinlive.com>
2026-01-31 23:26:53 -0800

    add: add frame icon component

---
+++ src/app/components/icons/add.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Add = () => {
+  return (
+    <svg
+      viewBox="0 0 18 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      preserveAspectRatio="xMidYMid meet"
+    >
+      <path d="M6.69347 0H11.1558V16H6.69347V0Z" fill="#828282" />
+      <path d="M0 10V6H17.8492V10H0Z" fill="#828282" />
+    </svg>
+  );
+};
+
+export default Add;
