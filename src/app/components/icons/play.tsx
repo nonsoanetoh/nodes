de2c404b7d661a1685da08d208f79c3a006470a5
Nonso Anetoh <anet0001@algonquinlive.com>
2026-01-31 23:26:49 -0800

    add: play icon component

---
+++ src/app/components/icons/play.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Play = () => {
+  return (
+    <svg
+      viewBox="0 0 16 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      preserveAspectRatio="xMidYMid meet"
+    >
+      <path
+        d="M15.0603 7.79424L9.53674e-07 15.5885V6.67572e-06L15.0603 7.79424Z"
+        fill="#595959"
+      />
+    </svg>
+  );
+};
+
+export default Play;
