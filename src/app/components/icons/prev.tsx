15480907b7020f5032b8f7a95202e1ceeb01d0f8
Nonso Anetoh <anet0001@algonquinlive.com>
2026-01-31 23:26:51 -0800

    add: previous frame icon component

---
+++ src/app/components/icons/prev.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Previous = () => {
+  return (
+    <svg
+      viewBox="0 0 18 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      preserveAspectRatio="xMidYMid meet"
+    >
+      <path d="M13.7926 0H17.8492V16H13.7926V0Z" fill="#595959" />
+      <path d="M0 8L12.1699 0V16L0 8Z" fill="#595959" />
+    </svg>
+  );
+};
+
+export default Previous;
